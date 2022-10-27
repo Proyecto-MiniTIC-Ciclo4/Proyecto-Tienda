d82145fec296a117cadd10ce919e5a29d4f872a9
Javier Diaz <jaedgodi20@gmail.com>
2022-10-26 22:52:11 -0500

    Vista admin productos

---
+++ tienda-informatica/src/components/productos-admin/tienda.js
@@ -0,0 +1,188 @@
+
+import {Fragment} from "react";
+function App() {
+  return (
+    <Fragment>
+    <h1>Pruba dia 26 del 10</h1>
+    <main>
+      <section class="py-1 text-center container">
+        <div class="row py-lg-2">
+          <div class="col-lg-7 col-md-8 mx-auto">
+            <h1 class="fw-light">Bienvenido a nuestra tienda de electronica</h1>
+            <p class="lead text-muted">
+              Para nosotros es un placer tenerte aqui, espero sea de tu agrado y
+              podamos tener pronto nuevamente
+            </p>
+            <p>
+              <a href="#" class="btn btn-primary my-2">
+                Carrito de compras
+              </a>
+              <br></br>
+              <a href="#" class="btn btn-secondary my-2">
+                Volver al Inicio
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card shadow-sm">
+                  <img src="https://clonesyperifericos.com/wp-content/uploads/2022/02/9-600x600.png"></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Nvidia RTX 3070 12Gb</h5>
+                    <p class="card-text">$ 6.990.000</p>
+                    <p class="card-text">
+                    tarjeta grafica 3070 de 12gb DDR6 utilizada para gaming profesional 
+                    es una maravilla lanzada en 2020
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="#" class="btn btn-primary">
+                          Descripcion
+                        </a>
+                      </div>
+                      <a href="#" class="btn btn-success">
+                        Agregar al carrito
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    </Fragment>
+  );
+}
+export default App;
