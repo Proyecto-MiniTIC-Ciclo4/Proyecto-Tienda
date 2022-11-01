9450c4159a88e9600791d26c6eecfdee3c74ea60
StevCorredor <Stevcorredor@gmail.com>
2022-11-01 17:19:45 -0500

    Adicion del encabezado a la vista Ventas Realizadas

--- tienda-informatica/src/components/ventas-realizadas-admin/ventas_Realizadas.js
+++ tienda-informatica/src/components/ventas-realizadas-admin/ventas_Realizadas.js
@@ -3,111 +3,139 @@ import React from "react";
 export function VentasRealizadas() {
     return (
         <div className="ventas_Realizadas">
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                        Software y Servicios JJEJ
-                    </a>
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a
-                                    class="nav-link active"
-                                    aria-current="page"
-                                    href="Lista_Productos"
-                                >
-                                    Lista Productos
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Modificar_Productos">
-                                    Modificar Productos
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Lista_Ventas">
-                                    Lista Ventas
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
             <container>
                 <main>
-                    <section class="py-1 text-center container">
-                        <h3 class="p-2 mt-2 h2 fw-bold">Ventas Realizadas </h3>
-                        <table class="table table-striped">
-                            <thead className="table-secondary">
-                                <tr>
-                                    <th>#</th>
-                                    <th>Fecha</th>
-                                    <th>id Venta</th>
-                                    <th>Valor</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>22/10/2022</td>
-                                    <td>123</td>
-                                    <td>$35.000</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>22/10/2022</td>
-                                    <td>456</td>
-                                    <td>$20.000</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>22/10/2022</td>
-                                    <td>789</td>
-                                    <td>$50.000</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>22/10/2022</td>
-                                    <td>110</td>
-                                    <td>$1.000</td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>22/10/2022</td>
-                                    <td>111</td>
-                                    <td>$1.000</td>
-                                </tr>
-                                <tr>
-                                    <td>6</td>
-                                    <td>22/10/2022</td>
-                                    <td>112</td>
-                                    <td>$1.000</td>
-                                </tr>
-                                <tr>
-                                    <td>7</td>
-                                    <td>22/10/2022</td>
-                                    <td>113</td>
-                                    <td>$1.000</td>
-                                </tr>
-                                <tr className="table-secondary">
-                                    <td colSpan={3}>Total Vendido</td>
-                                    <td>$110.000</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </section>
+                    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#">
+                                Tienda y electronica JJEJJ
+                            </a>
+                            <button
+                                class="navbar-toggler"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div
+                                class="collapse navbar-collapse"
+                                id="navbarSupportedContent"
+                            >
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a
+                                            class="nav-link active"
+                                            aria-current="page"
+                                            href="/"
+                                        >
+                                            Inicio
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">
+                                            Productos
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">
+                                            carrito de compras
+                                        </a>
+                                    </li>
+                                    <li class="nav-item disabled">
+                                        <a class="nav-link disabled" href="#">
+                                            Gracias por preferirnos
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul class="navbar-nav ms-auto">
+                                    <li class="nav-item mr-4">
+                                        <a href="#" class="nav-link disabled">
+                                            <i class="fas fa-user"></i>
+                                            <span>Bienvenido </span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <form method="post" action="/">
+                                            <a
+                                                href="#"
+                                                onclick="this.parentNode.submit();"
+                                                class="nav-link"
+                                            >
+                                                <i class="fas fa-sign-out-alt"></i>
+                                                Salir
+                                            </a>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
                 </main>
+                <section class="py-1 text-center container">
+                    <h3 class="p-2 mt-2 h2 fw-bold">Ventas Realizadas </h3>
+                    <table class="table table-striped">
+                        <thead className="table-secondary">
+                            <tr>
+                                <th>#</th>
+                                <th>Fecha</th>
+                                <th>id Venta</th>
+                                <th>Valor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>22/10/2022</td>
+                                <td>123</td>
+                                <td>$35.000</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>22/10/2022</td>
+                                <td>456</td>
+                                <td>$20.000</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>22/10/2022</td>
+                                <td>789</td>
+                                <td>$50.000</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>22/10/2022</td>
+                                <td>110</td>
+                                <td>$1.000</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>22/10/2022</td>
+                                <td>111</td>
+                                <td>$1.000</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>22/10/2022</td>
+                                <td>112</td>
+                                <td>$1.000</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>22/10/2022</td>
+                                <td>113</td>
+                                <td>$1.000</td>
+                            </tr>
+                            <tr className="table-secondary">
+                                <td colSpan={3}>Total Vendido</td>
+                                <td>$110.000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
             </container>
             <container>
                 <footer>
